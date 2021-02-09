00a65cdc1b43adbc37d7f544d94ad5cdc467a340
sakurait428 <sakurai_t@hotmail.com>
2021-02-09 21:52:52 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
     validates :user_birth_date
   end
 
-  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.' } do
     validates :first_name
     validates :last_name
   end
 
-  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
     validates :first_name_kana
     validates :last_name_kana
   end
