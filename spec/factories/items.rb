fff94d2100992a3c541758b82f91be781ba27b74
sakurait428 <sakurai_t@hotmail.com>
2021-02-13 14:28:54 +0900

    Faker使用

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name                    {'テスト'}
-    info                    {'テスト'}
-    category_id             {2}
-    sales_status_id         {2}
-    shipping_fee_status_id  {2}
-    prefecture_id           {2}
-    scheduled_delivery_id   {2}
-    price                   {300}
+    name                    {Faker::Name.name}
+    info                    {Faker::Name.name}
+    category_id             {Faker::Number.within(range: 2..11)}
+    sales_status_id         {Faker::Number.within(range: 2..7)}
+    shipping_fee_status_id  {Faker::Number.within(range: 2..3)}
+    prefecture_id           {Faker::Number.within(range: 2..48)}
+    scheduled_delivery_id   {Faker::Number.within(range: 2..4)}
+    price                   {Faker::Number.within(range: 300..9999999)}
     association :user
 
     after(:build) do |item|
