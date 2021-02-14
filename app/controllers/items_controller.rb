c7a3d4f020f30533a07984e7535529ee82a56214
sakurait428 <sakurai_t@hotmail.com>
2021-02-14 17:14:06 +0900

    ログアウト状態でも、商品一覧表示ページを見れる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all
