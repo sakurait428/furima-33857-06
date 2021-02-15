105ba4358ed19fc84b7b4f4d218cdd89d72aa5a6
sakurait428 <sakurai_t@hotmail.com>
2021-02-15 10:45:39 +0900

    詳細情報表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,12 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-#    @item = Item.find(params[:id])
-#    @category = Category.find(@item.category_id)
-#    @sales_status = SalesStatus.find(@item.sales_status_id)
-#    @shipping_fee_status = ShippingFeeStatus.find(@item.shipping_fee_status_id)
-#    @prefecture = Prefecture.find(@item.prefecture_id)
-#    @scheduled_delivery = ScheduledDelivery.find(@item.scheduled_delivery_id)
+    @item = Item.find(params[:id])
   end
 
   private
