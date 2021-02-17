e7cca9d302c787ec86a615d0df1990120f0aadb6
sakurait428 <sakurai_t@hotmail.com>
2021-02-17 10:22:35 +0900

    他人の商品は更新できなくする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -24,9 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless  current_user.id == @item.user.id
-      redirect_to root_path
-    end
+
   end
 
   def update
@@ -45,6 +44,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless  current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
 end
