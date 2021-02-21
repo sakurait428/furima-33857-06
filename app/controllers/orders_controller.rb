6752a05eb6cf3f1e95517775897c58c8f5a37779
sakurait428 <sakurai_t@hotmail.com>
2021-02-21 13:15:46 +0900

    オーダーモデル作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    @purchase_record_address = PurchaseRecordAddress.new
     @item = Item.find(params[:item_id])
   end
 
@@ -17,7 +18,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_record_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-  
+
 end
--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,13 +1,12 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_record
-
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id 
 
   def save
 
-    purchase_record = PurchaseRecord.create(user: user, item: item)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
 
-    Address.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, address: address, building: building, phone_number: phone_number, purchase_record: purchase_record)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
 
   end
 end
