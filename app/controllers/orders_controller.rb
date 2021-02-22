74fc6ad3099161a9897e18f6151f94c9163277b3
sakurait428 <sakurai_t@hotmail.com>
2021-02-22 21:51:32 +0900

    購入ページ　indexへ遷移させる記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :move_to_index_id_different, only: [:index, :create]
+  before_action :move_to_index_sold, only: [:index, :create]
 
   def index
     @purchase_record_address = PurchaseRecordAddress.new
@@ -8,9 +10,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-
     @purchase_record_address = PurchaseRecordAddress.new(order_params)
-    
     if @purchase_record_address.valid?
       pay_item
       @purchase_record_address.save
@@ -25,6 +25,21 @@ class OrdersController < ApplicationController
   def order_params
     @item = Item.find(params[:item_id])
     params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def move_to_index_id_different
+    item = Item.find(params[:item_id])
+    if  current_user.id == item.user.id
+      redirect_to root_path
+    end
+  end
+
+  def move_to_index_sold
+    purchase_record = PurchaseRecord.includes(:item)
+    item = Item.find(params[:item_id])
+    if purchase_record.exists?(item: item.id) == true
+      redirect_to root_path
+    end
   end
 
   def pay_item
